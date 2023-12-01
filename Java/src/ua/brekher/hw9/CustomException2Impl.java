980a2a90e898008919dccc7de43d3c1bda709a8a
anastasia <brehernasta@gmail.com>
2023-12-01 11:54:26 +0200

    commit

---
+++ Java/src/ua/brekher/hw9/CustomException2Impl.java
@@ -0,0 +1,7 @@
+package ua.brekher.hw9;
+
+public class ExceptionExample extends CustomException2 {
+    public CustomException2Impl(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ Java/src/ua/brekher/hw9/Exception.java
@@ -0,0 +1,19 @@
+package ua.brekher.hw9;
+
+public class ExceptionExample2 {
+    public static void main(String[] args) {
+        try {
+            throw new CustomException("Пример пользовательского исключения");
+        } catch (CustomException e) {
+            System.out.println("Поймано исключение: " + e.getMessage());
+        } finally {
+            System.out.println("Блок finally: Я был здесь!");
+        }
+    }
+}
+
+class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
