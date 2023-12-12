64b1a935ee54ee59f76e891f9c16b81bd1b51f26
Настя <149391331+qwerfdge@users.noreply.github.com>
2023-12-12 17:30:30 +0200

    Delete Java/src/ua/brekher/hw9 directory

--- Java/src/ua/brekher/hw9/CustomException2Impl.java
+++
@@ -1,7 +0,0 @@
-package ua.brekher.hw9;
-
-public class ExceptionExample extends CustomException2 {
-    public CustomException2Impl(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
