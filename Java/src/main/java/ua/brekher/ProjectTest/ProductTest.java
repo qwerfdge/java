4f6cae7ec18924adc1d449a15592e2363abcbd3b
anastasia <brehernasta@gmail.com>
2024-01-14 10:56:37 +0200

    commit 28.12.23

---
+++ Java/src/main/java/ua/brekher/ProjectTest/ProductTest.java
@@ -0,0 +1,21 @@
+package ua.brekher.ProjectTest;
+
+public class ProductTests {
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test(description = "Verify product details page displays correct information")
+    public void testProductDetails() {
+    }
+
+    @Test(description = "Verify product availability status")
+    public void testProductAvailability() {
+    }
+}
+
+
