f9472a4cc6b7b978a755d4c47bd8e0ad0acb226a
anastasia <brehernasta@gmail.com>
2024-01-14 13:11:00 +0200

    commit 28.12.23

---
+++ Java/src/main/java/ua/brekher/Project/LoginTest.java
@@ -0,0 +1,20 @@
+package ua.brekher.Project;
+mport org.testng.annotations.*;
+
+public class LoginTest {
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test(description = "Verify that a user can successfully log in with valid credentials")
+    public void testLoginWithValidCredentials() {
+    }
+
+    @Test(description = "Verify error message for invalid login")
+    public void testInvalidLoginErrorMessage() {
+    }
+}
---
+++ Java/src/main/java/ua/brekher/Project/ProductTest.java
@@ -0,0 +1,19 @@
+package ua.brekher.Project;
+
+public class ProductTests {
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test(description = "Verify product details page displays correct information")
+    public void testProductDetails() {
+    }
+
+    @Test(description = "Verify product availability status")
+    public void testProductAvailability() {
+    }
+}
