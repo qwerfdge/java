de1c5f959fd5c94b39c58746ae720efc4a1acb13
anastasia <brehernasta@gmail.com>
2024-01-24 15:08:04 +0200

    commit 24.01.24

---
+++ Java/src/main/java/ua/brekher/hw46/GitHubApiTest.java
@@ -0,0 +1,31 @@
+package main.java.ua.brekher.hw46;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.http.HttpClient;
+import org.openqa.selenium.remote.http.HttpRequest;
+
+import java.net.URI;
+
+public class GitHubApiTest {
+
+    @Test
+    <JSONObject>
+    void testGitHubApiWithHttpClient() throws Exception {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+
+        HttpRequest request;
+        request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com/emojis"))
+                .build();
+
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        JSONObject jsonResponse = new JSONObject(response.body());
+
+
+        assertTrue(jsonResponse.has("articulated_lorry"));
+    }
